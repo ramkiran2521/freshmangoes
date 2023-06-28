47e794c0183a7cd0aa059f5c655a4b044f4acff4
Kiran kumar R <kirankkr502@gmail.com>
2023-06-28 19:31:32 +0530

    designed product component

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -3,85 +3,121 @@ import "./Product.css";
 import { useLocation } from "react-router-dom";
 import CartContext from "../../store/CartContext";
 
-const Product = (props) => {
-  const state = useLocation();
+const Product = () => {
+  const { state } = useLocation();
   const cartContext = useContext(CartContext);
 
-  const [counter, setCounter] = useState(1);
-  const [isDisabled, setIsDisabled] = useState(true);
+  let quantity = 0;
+  const thisElement = cartContext.items.findIndex((ele) => {
+    return ele.id === state.product._id;
+  });
+
+  if (thisElement >= 0) {
+    quantity = cartContext.items[thisElement].quantity;
+  }
+
+  const [isDecBtnDisabled, setIsDecBtnDisabled] = useState(true);
 
   function isDisabledFun(co) {
-    if (co < 2) {
-      setIsDisabled(true);
+    if (co <= 0) {
+      setIsDecBtnDisabled(true);
     } else {
-      setIsDisabled(false);
+      setIsDecBtnDisabled(false);
     }
   }
 
-  function decrimentCounter() {
-    setCounter((prev) => {
-      isDisabledFun(prev - 1);
-      return prev - 1;
-    });
+  function prevent(e) {
+    e.stopPropagation();
   }
 
-  function incrementCounter() {
-    setCounter((prev) => {
-      isDisabledFun(prev + 1);
-      return prev + 1;
-    });
+  function removeFromCart() {
+    cartContext.removeItem(state.product._id);
+    isDisabledFun(quantity - 1);
   }
 
   function addToCart() {
-
     cartContext.addItem({
-      id: state.state.product._id,
-      imgurl: state.state.product.imageurl,
-      pName: state.state.product.productname,
-      ogPrice: state.state.product.originalprice,
-      offPrice: state.state.product.offerprice,
-      quantity: counter,
+      id: state.product._id,
+      imgurl: state.product.imageurl,
+      pName: state.product.productname,
+      ogPrice: state.product.originalprice,
+      offPrice: state.product.offerprice,
+      quantity: 1,
     });
-
-    setCounter(1);
-    setIsDisabled(true);
+    isDisabledFun(quantity + 1);
   }
 
   return (
-    <div
-      className={`each-card ${props.className}`}
-      id={state.state.product._id}
-    >
-      <div className="card-img">
-        <img
-          src={state.state.product.imageurl}
-          alt="image of mangoes"
-        ></img>
-      </div>
-      <div className="card-des">
-        <h4 className="pro-name">
-          {state.state.product.productname}
-        </h4>
-        <div className="cost">
-          <span id="original-cost">
-            ₹{state.state.product.originalprice}
-          </span>
-          <span className="offer-cost">
-            {" "}
-            ₹{state.state.product.offerprice}
-          </span>
+    <div className="product-cont">
+      <div>
+        <h3>
+          <a href="/">Home</a> / {state.product.productname}
+        </h3>
+        <div className="pr-details">
+          <div className="pr-img">
+            <div>
+              <img src={state.product.imageurl} />
+            </div>
+            <div
+              className={`add-cart-btn ${
+                quantity ? "item-added" : ""
+              } `}
+              onClick={prevent}
+            >
+              <button
+                className="minus-btn"
+                onClick={removeFromCart}
+                disabled={isDecBtnDisabled}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  height="1em"
+                  viewBox="0 0 448 512"
+                >
+                  <path d="M432 256c0 17.7-14.3 32-32 32L48 288c-17.7 0-32-14.3-32-32s14.3-32 32-32l352 0c17.7 0 32 14.3 32 32z" />
+                </svg>
+              </button>
+              <span className="hide-quantity">{quantity}</span>
+              <button className="plus-btn" onClick={addToCart}>
+                <span>Add to Cart</span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  height="1em"
+                  viewBox="0 0 448 512"
+                >
+                  <path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
+                </svg>
+              </button>
+            </div>
+          </div>
+          <div className="pr-details-des">
+            <h2>{state.product.productname}</h2>
+            <div className="p-cost">
+              {state.product.originalprice !==
+                state.product.offerprice && (
+                <span className="og-price">
+                  ₹{state.product.originalprice}
+                </span>
+              )}
+              <span className="off-price">
+                {" "}
+                ₹{state.product.offerprice}
+              </span>
+              <p>₹{state.product.offerprice} / 3kg</p>
+            </div>
+            <div className="pr-det-text">
+              <h4>Product Details</h4>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing
+                elit. Maecenas massa erat, pharetra vitae mi eu,
+                tincidunt imperdiet justo. Cras venenatis sit amet
+                tellus vel condimentum. Etiam lacinia lectus velit, at
+                pulvinar enim suscipit et.
+              </p>
+            </div>
+            <a href="/">Shop More...</a>
+          </div>
         </div>
-        <span>₹300 / 3kg</span>
-        <div className="quantity">
-          <button onClick={decrimentCounter} disabled={isDisabled}>
-            -
-          </button>
-          <span>{counter}</span>
-          <button onClick={incrementCounter}>+</button>
-        </div>
-        <button className="add-cart-btn" onClick={addToCart}>
-          Add to Cart
-        </button>
       </div>
     </div>
   );
